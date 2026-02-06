e61c188e5695a8c62d04dae5bbf1506b502f5fb5
Fateme100 <f.aghaei1377@gmail.com>
2026-02-06 15:25:31 +0330

    Update and rename exercise_1 (9).py to exercise_15.py

--- exercise_15.py
+++ exercise_15.py
@@ -31,4 +31,5 @@ def Bubble(A):
     for i in range(len(A)-1):
         for j in range(len(A)-1):
             if A[j] > A[j+1]:
+
                 A[j], A[j+1] = A[j+1] , A[j]                       
